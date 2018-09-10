5d336fa7a6ad113d5ef3b22240159063cc14ed76
eldc <eldc@paypal.com>
2018-09-10 09:49:06 -0700

    remove unwanted statements

--- client/index.js
+++ client/index.js
@@ -3,8 +3,6 @@ import React from 'react'
 import { hydrate } from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from '../common/store/configureStore'
-// import App from '../common/containers/App'
-// import App1 from '../common/containers/App1'
 import App from '../common/components/App';
 
 const preloadedState = window.__PRELOADED_STATE__
